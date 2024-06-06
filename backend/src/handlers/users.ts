ebf61a54bbf1e3265847a72beea3fd52a786199b
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2024-06-06 19:10:27 +0200

    /users

---
+++ backend/src/handlers/users.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express-serve-static-core";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+import { users, User } from "../db/schema";
+
+export async function getUsers(req: Request, res: Response) {
+  let allUsers: User[] = await db.select().from(users);
+  res.send(allUsers);
+}
+
+export async function postUser(req: Request, res: Response) {
+  const newUser: User = req.body;
+
+  try {
+    await db.insert(users).values(newUser);
+    res.send("User added");
+  } catch (err) {
+    res.status(500).send("Error adding user");
+  }
+}
+
+export async function getUser(req: Request, res: Response) {
+  const { id } = req.params;
+
+  let user: User[] = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, Number(id)));
+
+  if (user.length > 0) {
+    res.send(user[0]);
+  } else {
+    res.status(404).send("User not found");
+  }
+}
+
+export async function updateUser(req: Request, res: Response) {
+  const { id } = req.params;
+  const updateFields: Partial<User> = req.body;
+
+  try {
+    await db
+      .update(users)
+      .set(updateFields)
+      .where(eq(users.id, Number(id)));
+    res.send("User updated");
+  } catch (err) {
+    res.status(500).send("Error updating user");
+  }
+}
+
+// TODO: Implement deleteUser
+
+// export async function deleteUser(req: Request, res: Response) {
+//   const { id } = req.params;
+
+//   try {
+//     await db.delete(users).where(eq(users.id, Number(id)));
+//     res.send("User deleted");
+//   } catch (err) {
+//     res.status(500).send("Error deleting user");
+//   }
+// }
