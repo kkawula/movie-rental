b91f21ab9f075bef6c35f94f13cdfccfb733befc
Krzysztof Ligarski <kligarski@student.agh.edu.pl>
2024-06-08 23:43:20 +0200

    Fixed some bugs and added incomplete handling of multiple genre_ids

--- backend/src/handlers/movies.ts
+++ backend/src/handlers/movies.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express-serve-static-core";
 import { db } from "../db";
-import { Movie, NewMovie, movies, moviesAvailabilityView } from "../db/schema";
-import { SQLWrapper, eq, and, ilike, gte, lte, gt, desc } from "drizzle-orm";
+import { Movie, NewMovie, movies, moviesAvailabilityView, moviesgenres } from "../db/schema";
+import { SQLWrapper, eq, and, ilike, gte, lte, gt, desc, or } from "drizzle-orm";
 
 export async function getMovie(req: Request, res: Response) {
   const { id } = req.params;
@@ -23,7 +23,7 @@ export async function getMovie(req: Request, res: Response) {
 }
 
 export async function getMovies(req: Request, res: Response) {
-  // TODO: what about genre_id? -> allow multiple?
+  const { genre_ids } = req.body;
   const { availability, title, description, imdb_gte, imdb_lte, director } = req.query; 
   const moviesAvailabilityColumns = {
     id: moviesAvailabilityView.id,
@@ -35,32 +35,48 @@ export async function getMovies(req: Request, res: Response) {
   }
   
   let query;
+  let chosenTable;
   const filters: SQLWrapper[] = [];
   try {
     if (availability === "true") {
+      chosenTable = moviesAvailabilityView;
       query = db.select(moviesAvailabilityColumns).from(moviesAvailabilityView);
       filters.push(gt(moviesAvailabilityView.available, 0));
     } else if (availability === "false") {
+      chosenTable = moviesAvailabilityView;
       query = db.select(moviesAvailabilityColumns).from(moviesAvailabilityView);
       filters.push(eq(moviesAvailabilityView.available, 0));
     } else {
+      chosenTable = movies;
       query = db.select().from(movies);
     }
   
     if (title) {
-      filters.push(ilike(movies.title, `%${title}%`));
+      filters.push(ilike(chosenTable.title, `%${title}%`));
     }
     if (description) {
-      filters.push(ilike(movies.description, `%${description}%`));
+      filters.push(ilike(chosenTable.description, `%${description}%`));
     }
     if (director) {
-      filters.push(ilike(movies.director, `%${director}%`));
+      filters.push(ilike(chosenTable.director, `%${director}%`));
     }
     if (imdb_gte) {
-      filters.push(gte(movies.imdb_rate, imdb_gte.toString()));
+      filters.push(gte(chosenTable.imdb_rate, imdb_gte.toString()));
     }
     if (imdb_lte) {
-      filters.push(lte(movies.imdb_rate, imdb_lte.toString()));
+      filters.push(lte(chosenTable.imdb_rate, imdb_lte.toString()));
+    }
+
+    if (genre_ids) {
+      query.innerJoin(moviesgenres, eq(chosenTable.id, moviesgenres.movie_id));
+      if (Array.isArray(genre_ids)) {
+        // TODO: this is basically an OR of genres. To do AND you need to use grouping and count().
+        const genreFilters: SQLWrapper[] = [];
+        genre_ids.forEach((genre_id) => genreFilters.push(eq(moviesgenres.genre_id, Number(genre_id))));
+        filters.push(or(...genreFilters) as SQLWrapper);
+      } else {
+        filters.push(eq(moviesgenres.genre_id, genre_ids));
+      }
     }
   
     let requestedMovies: Movie[] = await query.where(and(...filters));
