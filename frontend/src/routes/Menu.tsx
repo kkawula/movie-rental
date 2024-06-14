727a18c3dc478f88a2992cad819128e9c9555f94
Krzysztof Ligarski <kligarski@student.agh.edu.pl>
2024-06-15 00:27:18 +0200

    Added CORS, changed frontend to use Vite with Mantine. Created basic layout

---
+++ frontend/src/routes/Menu.tsx
@@ -0,0 +1,59 @@
+import { UnstyledButton } from "@mantine/core";
+import { NavLink } from "react-router-dom";
+import classes from "./Layout.module.css";
+
+interface MenuProps {
+    onClick?: () => void
+}
+
+export default function Menu({ onClick }: MenuProps) {
+  return (
+    <>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? classes.control_active : classes.control
+        }
+        to={"/"}
+        onClick={onClick ? onClick : undefined}
+      >
+        <UnstyledButton>Home</UnstyledButton>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? classes.control_active : classes.control
+        }
+        to={"/users"}
+        onClick={onClick ? onClick : undefined}
+      >
+        <UnstyledButton>Clients</UnstyledButton>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? classes.control_active : classes.control
+        }
+        to={"/movies"}
+        onClick={onClick ? onClick : undefined}
+      >
+        <UnstyledButton>Movies</UnstyledButton>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? classes.control_active : classes.control
+        }
+        to={"/rentals"}
+        onClick={onClick ? onClick : undefined}
+      >
+        <UnstyledButton>Rentals</UnstyledButton>
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive ? classes.control_active : classes.control
+        }
+        to={"/reports"}
+        onClick={onClick ? onClick : undefined}
+      >
+        <UnstyledButton>Reports</UnstyledButton>
+      </NavLink>
+    </>
+  );
+}
